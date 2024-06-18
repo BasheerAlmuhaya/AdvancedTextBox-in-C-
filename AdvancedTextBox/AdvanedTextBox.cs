e055d248a4349bea5d4a45569188b798b9bda9cf
BasheerAlmohia <63898020+BasheerAlmohia@users.noreply.github.com>
2024-06-18 23:01:18 +0530

    first commit

---
+++ AdvancedTextBox/AdvanedTextBox.cs
@@ -0,0 +1,231 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+using System.Drawing.Drawing2D;
+using System.Drawing;
+
+
+namespace AdvancedTextBox
+{
+    class AdvanedTextBox : TextBox
+    {
+
+        private Pen penBorder;
+        private Color IdelColor = Color.DarkGray;
+        private Color FocusedColor = Color.Red;
+        private String placeHolder = "";
+        private Color OldForColor = Color.Black;
+        private bool IsFocused = false;
+        private bool isPassword = false;
+        private String passwordChart = "";
+
+
+
+        public AdvanedTextBox()
+            : base()
+        {
+            SetStyle(ControlStyles.UserPaint, true);
+            OldForColor = this.ForeColor;
+            this.BorderStyle = System.Windows.Forms.BorderStyle.Fixed3D;
+            this.Font = new Font("Times New Roman", 12, FontStyle.Regular);
+            
+        }
+
+        protected override void OnPaint(PaintEventArgs e)
+        {
+            base.OnPaint(e);
+            if (IsFocused)
+            SetFocusedBorder(e);
+            else
+                SetIdelBorder(e);
+        }
+
+        protected override void OnEnter(EventArgs e)
+        {
+            IsFocused = true;
+            
+            if (this.Text == PlaceHolder)
+            {
+                this.Text = "";
+                this.ForeColor = OldForColor;
+            }
+            Refresh();
+            base.OnEnter(e);
+            
+        }
+
+        protected override void OnLeave(EventArgs e)
+        {
+            IsFocused = false;
+
+            if (this.Text == "")
+            {
+                this.Text = placeHolder;
+                this.ForeColor = Color.LightGray;
+            }
+            else
+            {
+                this.ForeColor = OldForColor;
+            }
+            
+            base.OnLeave(e);
+            Refresh();
+        }
+
+        protected override void OnKeyPress(KeyPressEventArgs e)
+{
+
+
+    if (isPassword)
+    {
+
+        if (e.KeyChar.GetHashCode().ToString() == "524296")
+        {
+            if (Tag.ToString().Length != 0)
+            {
+                Tag = Tag.ToString().Substring(0, Tag.ToString().Length - 1);
+                Text = "";
+                for (int i = 0; i < Tag.ToString().Length; i++)
+                {
+                    Text += passwordChart;
+                }
+            }
+            return;
+        }
+        else if (e.KeyChar.GetHashCode().ToString() == "851981") return;
+
+
+        Tag += e.KeyChar.ToString();
+        e.Handled = true;
+        Text = "";
+        for (int i = 0; i < Tag.ToString().Length; i++)
+        {
+            Text += passwordChart;
+        }
+
+
+    }
+
+}
+
+
+        private void SetFocusedBorder(PaintEventArgs e)
+        {
+            penBorder = new Pen(FocusedColor, 1);
+            Rectangle rectBorder = new Rectangle(e.ClipRectangle.X, e.ClipRectangle.Y, e.ClipRectangle.Width - 1, e.ClipRectangle.Height - 1);
+            e.Graphics.DrawRectangle(penBorder, rectBorder);
+            Rectangle textRect = new Rectangle(e.ClipRectangle.X+1 , e.ClipRectangle.Y+1 , e.ClipRectangle.Width - 2, e.ClipRectangle.Height - 2);
+            e.Graphics.DrawString(Text, Font, Brushes.Black, textRect);
+            //TextRenderer.DrawText(e.Graphics, Text, Font, textRect, this.ForeColor, this.BackColor, TextFormatFlags.ModifyString);
+        }
+
+        private void SetIdelBorder(PaintEventArgs e)
+        {
+            penBorder = new Pen(IdelColor, 1);  
+            Rectangle rectBorder = new Rectangle(e.ClipRectangle.X , e.ClipRectangle.Y , e.ClipRectangle.Width-1, e.ClipRectangle.Height-1);
+            e.Graphics.DrawRectangle(penBorder, rectBorder);
+            
+            Rectangle textRect = new Rectangle(e.ClipRectangle.X+1, e.ClipRectangle.Y+1, e.ClipRectangle.Width - 2, e.ClipRectangle.Height - 2);
+            e.Graphics.DrawString(Text, Font, Brushes.Black, textRect);
+            //TextRenderer.DrawText(e.Graphics, Text, Font, textRect, this.ForeColor, this.BackColor, TextFormatFlags.ModifyString);
+        }
+
+
+
+
+
+        public Color IdelBorderColor
+        {
+            set
+            {
+                IdelColor = value;
+                Refresh();
+            }
+
+            get
+            {
+                return IdelColor;
+            }
+        }
+
+        public Color FocusedlBorderColor
+        {
+            set
+            {
+                FocusedColor = value;
+                Refresh();
+            }
+
+            get
+            {
+                return FocusedColor;
+            }
+        }
+
+        public String PlaceHolder
+        {
+            set
+            {
+                placeHolder = value;
+                Text = placeHolder;
+                Refresh();
+            }
+
+            get
+            {
+                return placeHolder;
+            }
+        }
+        
+        public String Designer
+        {
+            get
+            {
+                return "Emran Al Hadad";
+            }
+            
+        }
+
+        public String MyPasswordChart
+        {
+
+            set
+            {
+                passwordChart = value;
+            }
+
+            get
+            {
+                return passwordChart;
+            }
+        }
+
+        public bool ISPassword
+        {
+
+            set
+            {
+                isPassword = value;
+            }
+
+            get
+            {
+                return isPassword;
+            }
+        }
+
+        public override bool Multiline
+        {
+            get
+            {
+                return base.Multiline;
+            }
+            
+        }
+}
+        }
+    
+
---
+++ AdvancedTextBox/Form1.Designer.cs
@@ -0,0 +1,136 @@
+﻿namespace AdvancedTextBox
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.advanedTextBox3 = new AdvancedTextBox.AdvanedTextBox();
+            this.advanedTextBox2 = new AdvancedTextBox.AdvanedTextBox();
+            this.advanedTextBox1 = new AdvancedTextBox.AdvanedTextBox();
+            this.textBox1 = new System.Windows.Forms.TextBox();
+            this.SuspendLayout();
+            // 
+            // advanedTextBox3
+            // 
+            this.advanedTextBox3.FocusedlBorderColor = System.Drawing.Color.Red;
+            this.advanedTextBox3.Font = new System.Drawing.Font("Times New Roman", 12F);
+            this.advanedTextBox3.IdelBorderColor = System.Drawing.Color.DarkGray;
+            this.advanedTextBox3.ISPassword = true;
+            this.advanedTextBox3.Location = new System.Drawing.Point(92, 103);
+            this.advanedTextBox3.Margin = new System.Windows.Forms.Padding(3, 4, 3, 4);
+            this.advanedTextBox3.MyPasswordChart = "#";
+            this.advanedTextBox3.Name = "advanedTextBox3";
+            this.advanedTextBox3.PlaceHolder = "Password";
+            this.advanedTextBox3.Size = new System.Drawing.Size(238, 30);
+            this.advanedTextBox3.TabIndex = 2;
+            this.advanedTextBox3.Text = "Password";
+            // 
+            // advanedTextBox2
+            // 
+            this.advanedTextBox2.FocusedlBorderColor = System.Drawing.Color.Red;
+            this.advanedTextBox2.Font = new System.Drawing.Font("Times New Roman", 12F);
+            this.advanedTextBox2.IdelBorderColor = System.Drawing.Color.DarkGray;
+            this.advanedTextBox2.ISPassword = false;
+            this.advanedTextBox2.Location = new System.Drawing.Point(92, 143);
+            this.advanedTextBox2.Margin = new System.Windows.Forms.Padding(3, 4, 3, 4);
+            this.advanedTextBox2.MyPasswordChart = "";
+            this.advanedTextBox2.Name = "advanedTextBox2";
+            this.advanedTextBox2.PlaceHolder = "";
+            this.advanedTextBox2.Size = new System.Drawing.Size(238, 30);
+            this.advanedTextBox2.TabIndex = 1;
+            // 
+            // advanedTextBox1
+            // 
+            this.advanedTextBox1.FocusedlBorderColor = System.Drawing.Color.Red;
+            this.advanedTextBox1.Font = new System.Drawing.Font("Times New Roman", 12F);
+            this.advanedTextBox1.IdelBorderColor = System.Drawing.Color.Blue;
+            this.advanedTextBox1.ISPassword = false;
+            this.advanedTextBox1.Location = new System.Drawing.Point(92, 64);
+            this.advanedTextBox1.Margin = new System.Windows.Forms.Padding(3, 4, 3, 4);
+            this.advanedTextBox1.MyPasswordChart = "";
+            this.advanedTextBox1.Name = "advanedTextBox1";
+            this.advanedTextBox1.PlaceHolder = "UserName";
+            this.advanedTextBox1.Size = new System.Drawing.Size(238, 30);
+            this.advanedTextBox1.TabIndex = 0;
+            this.advanedTextBox1.Text = "UserName";
+            // 
+            // textBox1
+            // 
+            this.textBox1.Location = new System.Drawing.Point(92, 215);
+            this.textBox1.Margin = new System.Windows.Forms.Padding(3, 4, 3, 4);
+            this.textBox1.Name = "textBox1";
+            this.textBox1.Size = new System.Drawing.Size(238, 24);
+            this.textBox1.TabIndex = 3;
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(7F, 16F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.BackColor = System.Drawing.Color.White;
+            this.ClientSize = new System.Drawing.Size(416, 273);
+            this.Controls.Add(this.textBox1);
+            this.Controls.Add(this.advanedTextBox3);
+            this.Controls.Add(this.advanedTextBox2);
+            this.Controls.Add(this.advanedTextBox1);
+            this.Margin = new System.Windows.Forms.Padding(3, 4, 3, 4);
+            this.Name = "Form1";
+            this.Text = "  ";
+            this.Load += new System.EventHandler(this.Form1_Load);
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private AdvanedTextBox advanedTextBox1;
+        private AdvanedTextBox advanedTextBox2;
+        private AdvanedTextBox advanedTextBox3;
+        private System.Windows.Forms.TextBox textBox1;
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+    }
+}
+
